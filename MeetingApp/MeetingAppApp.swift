642cf1841570fc88cf78523aca791e4ca6d308d5
aditya7496 <43040075+aditya7496@users.noreply.github.com>
2024-08-01 12:53:51 -0300

    Initial Commit

---
+++ MeetingApp/MeetingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingAppApp.swift
+//  MeetingApp
+//
+//  Created by Aditya Agrawal on 2024-08-01.
+//
+
+import SwiftUI
+
+@main
+struct MeetingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
